f7665f6de8cb057da2c0454ebe2c7fa3865c2ca0
ShauryaveerChauhan <shauryaveerchauhan26@gmail.com>
2025-05-23 18:25:54 +0000

    backend done

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -2,6 +2,9 @@ import { prismaClient } from "@/app/lib/db";
 import { url } from "inspector";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+// @ts-ignore
+import youtubesearchapi from "youtube-search-api"
+
 const YouTube_RegEx = new RegExp("^https:\/\/www\.youtube\.com\/watch\?v=[\w-]{11}$")
 
 const CreateStreamSchema = z.object({
@@ -22,14 +25,26 @@ export async function POST(req: NextRequest){
             }
 
             const extractedId = data.url.split("v=?")[1]
+            
+           const res =  youtubesearchapi.GetVideoDetails(extractedId)
 
-           await prismaClient.stream.create({
+            console.log(res.title);
+            console.log(res.thumbnail)
+            console.log(JSON.stringify(res.thumbnail.thumbnails))
+            const thumbnails = res.thumbnail.thumbnails
+            thumbnails.sort((a: {width : number}, b : {width : number}) => a.width < b.width ? -1 : 1)
+
+          const stream =   await prismaClient.stream.create({
             data: {
             userId: data.creatorId,
             url: data.url,
             extractedId,
-            type: "Youtube"
-            }})
+            type: "Youtube",
+            title: res.title,
+            // @ts-ignore
+            smallImg : (thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? "no thumbnail found :(" ,
+            bigImg : thumbnails[thumbnails.length].url
+            }}) ?? "no thumbnail found :("
     }
 catch(e){
       return NextResponse.json({
@@ -45,7 +60,7 @@ export async function GET(req : NextRequest){
     const creatorId = req.nextUrl.searchParams.get("creatorId");
     const streams = await prismaClient.user.findMany({
         where : {
-            userId : creatorId ?? ""
+            id : creatorId ?? ""
 
         }
     })
